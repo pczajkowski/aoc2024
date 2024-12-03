cb095db76d191a16001cb87e9cf21faab5759872
Piotr Czajkowski <pczajkowski@gmail.com>
2024-12-03 11:11:25 +0100

    Solved for sample, clunky

--- 03/code.go
+++ 03/code.go
@@ -6,17 +6,21 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strings"
 )
 
-func readInput(file *os.File) [][]int {
+func readInput(file *os.File) ([][]int, []string) {
 	scanner := bufio.NewScanner(file)
 	var muls [][]int
+	var lines []string
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
 			break
 		}
+
+		lines = append(lines, line)
 
 		re := regexp.MustCompile(`mul\(\d+,\d+\)`)
 		matches := re.FindAllString(line, -1)
@@ -31,13 +35,60 @@ func readInput(file *os.File) [][]int {
 		}
 	}
 
-	return muls
+	return muls, lines
 }
 
 func part1(muls [][]int) int {
 	var result int
 	for _, mul := range muls {
 		result += mul[0] * mul[1]
+	}
+
+	return result
+}
+
+func part2(lines []string) int {
+	var result int
+	multiply := true
+	re := regexp.MustCompile(`mul\(\d+,\d+\)`)
+
+	var startIndex int
+	for _, line := range lines {
+		edge := len(line) - 1
+		endIndex := edge
+		reading := true
+		for reading {
+			if multiply {
+				index := strings.Index(line[startIndex:edge], "don't()")
+				multiply = false
+				if index == -1 {
+					endIndex = edge
+					reading = false
+				} else {
+					endIndex = index
+				}
+
+				matches := re.FindAllString(line[startIndex:endIndex], -1)
+				for _, match := range matches {
+					mul := make([]int, 2)
+					n, err := fmt.Sscanf(match, "mul(%d,%d)", &mul[0], &mul[1])
+					if n != 2 || err != nil {
+						log.Fatalf("Bad input: %s", err)
+					}
+
+					result += mul[0] * mul[1]
+				}
+			} else {
+				index := strings.Index(line[startIndex:edge], "do()")
+				if index == -1 {
+					startIndex = 0
+					reading = false
+				} else {
+					multiply = true
+					startIndex = index
+				}
+			}
+		}
 	}
 
 	return result
@@ -54,6 +105,7 @@ func main() {
 		log.Fatalf("Failed to open %s!\n", filePath)
 	}
 
-	muls := readInput(file)
+	muls, lines := readInput(file)
 	fmt.Println("Part1:", part1(muls))
+	fmt.Println("Part2:", part2(lines))
 }

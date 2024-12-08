283a81136c6ddf04cb31d58835566207a03a401b
Piotr Czajkowski <pczajkowski@gmail.com>
2024-12-08 17:26:45 +0100

    Solved part1

--- 08/code.go
+++ 08/code.go
@@ -40,6 +40,52 @@ func readInput(file *os.File) (map[byte][]Point, [][]byte) {
 	return groups, matrix
 }
 
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+
+	return a
+}
+
+func checkAntinode(antinode Point, matrix [][]byte, key byte) bool {
+	if antinode.x < 0 || antinode.y < 0 || antinode.y >= len(matrix) || antinode.x >= len(matrix[0]) {
+		return false
+	}
+
+	if matrix[antinode.y][antinode.x] == key {
+		return false
+	}
+
+	return true
+}
+
+func getAntinodes(groups map[byte][]Point, matrix [][]byte) map[string]Point {
+	antinodes := make(map[string]Point)
+
+	for key, items := range groups {
+		edge := len(items)
+		for i := range items {
+			for j := i + 1; j < edge; j++ {
+				deltaX := items[j].x - items[i].x
+				deltaY := items[j].y - items[i].y
+
+				firstAntinode := Point{x: items[i].x - deltaX, y: items[i].y - deltaY}
+				if checkAntinode(firstAntinode, matrix, key) {
+					antinodes[firstAntinode.key()] = firstAntinode
+				}
+
+				secondAntinode := Point{x: items[j].x + deltaX, y: items[j].y + deltaY}
+				if checkAntinode(secondAntinode, matrix, key) {
+					antinodes[secondAntinode.key()] = secondAntinode
+				}
+			}
+		}
+	}
+
+	return antinodes
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("You need to specify a file!")
@@ -52,5 +98,6 @@ func main() {
 	}
 
 	groups, matrix := readInput(file)
-	fmt.Println(groups, matrix)
+	antinodes := getAntinodes(groups, matrix)
+	fmt.Println("Part1:", len(antinodes))
 }
